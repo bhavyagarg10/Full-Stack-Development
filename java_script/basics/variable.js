f6de2b23a367b82424157ebd92bcfffed5adbae3
bhavyagarg10 <bhavyagarg310@gmail.com>
2024-12-12 22:21:26 +0530

    variables in js

---
+++ java_script/basics/variable.js
@@ -0,0 +1,14 @@
+const account=121654651 //value never change
+let name="Bhavya"
+var pass="23135"
+state="Uttar Pradesh"
+let city;
+
+//now change the values
+name="Hitesh"
+pass="564521"
+state="Bihar"
+
+console.log(account);
+console.table([name,pass,state,city])// print all the given variable in a table
+
