679e293b2ebb7ed362ff978fbfa2853c5b37527e
bhavyagarg10 <bhavyagarg310@gmail.com>
2024-12-15 10:18:01 +0530

    type conversion

---
+++ java_script/basics/03_conversionops.js
@@ -0,0 +1,35 @@
+/*let score="33"
+let score1="33abc"
+let score2="null"
+let score3="undefined"
+let score4=true
+
+console.log(typeof score,score);
+console.log(typeof score1,score1);
+console.log(typeof score2,score2);
+console.log(typeof score3,score3);
+console.log(typeof score4,score4);
+
+let cscore=Number(score)
+let cscore1=Number(score1)
+let cscore2=Number(score2)
+let cscore3=Number(score3)
+let cscore4=Number(score4)
+
+console.log(typeof cscore , cscore)
+console.log(typeof cscore1 , cscore1)
+console.log(typeof cscore2 , cscore2)
+console.log(typeof cscore3 , cscore3)
+console.log(typeof cscore4 , cscore4)*/
+
+/*let a=1
+console.log(Boolean(a)) //true
+let b=""
+console.log(Boolean(b)) //false
+let c="Bhavya"
+console.log(Boolean(c)) //true*/
+
+/*let a=23
+let ca=String(a)
+console.log(typeof ca,ca);*/
+
