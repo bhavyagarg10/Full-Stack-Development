24644a9980847373b0e27a8aa3b80c30dae5f6b0
bhavyagarg10 <bhavyagarg310@gmail.com>
2024-12-18 23:17:11 +0530

    numbers and Math in js

---
+++ java_script/basics/06_number_and_maths.js
@@ -0,0 +1,32 @@
+// const n=100
+// const n1=new Number(100)
+// console.log((n1));
+// console.log(n1.toFixed(2));
+// console.log(n1.toString());
+// console.log(n1.toString().length);
+
+// const num=123.456
+// console.log(num.toPrecision(4));
+// console.log(num.toPrecision(3));
+
+// const amount=10000000
+// console.log(amount.toLocaleString());
+
+// +++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.sqrt(25));
+// console.log(Math.max(5,14,6));
+// console.log(Math.min(5,14,6));
+
+// Random
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
